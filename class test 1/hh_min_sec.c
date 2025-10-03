13bb1eb958816e9ec5d10cca792290554b8d0016
ameyvaidya18 <ameyvaidya9372@gmail.com>
2025-10-03 11:21:00 +0530

    Add files via upload

---
+++ class test 1/hh_min_sec.c
@@ -0,0 +1,10 @@
+void main(){
+
+int hh=2,min=26,sec=45;
+int totalsec;
+hh=2*60*60;
+min=26*60;
+totalsec = hh+min+sec;
+
+printf("totalsec = %d",totalsec);
+}
---
+++ class test 1/leapyear.c
@@ -0,0 +1,9 @@
+void main()
+{
+	int y=2016;
+	if(y%4==0 ||y%400==0 && y%100==0 )
+	printf("leap year");
+	else
+	printf("not a leap year");
+	
+}
